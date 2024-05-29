8ca025e0c89c0f5999e9f20fa7fbccbd0c0b6958
thongh20 <tungnguyen10i2vh@gmail.com>
2024-05-29 22:42:57 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+
+Console.InputEncoding = Encoding.UTF8; 
+
+
+
+
+Console.WriteLine("mời bạn nhập tên vào");
+string names = Console.ReadLine();
+//Console.WriteLine($"teen cua ban la : {names}");
+Console.WriteLine(names);
+string tuoi= Console.ReadLine();
+Console.WriteLine("mời bạn nhập tuổi vào : ");
+Console.WriteLine(tuoi);
+//Console.WriteLine($"tuổi của bạn là {tuoi}");
+string diachi = Console.ReadLine();
+Console.WriteLine(diachi);
+//Console.WriteLine($"địa chỉ của bạn là {diachi} ");
+Console.WriteLine($"tên của bạn là  {names}, Tuối{tuoi},địa chỉ của bạm{diachi}");
+
+Console.ReadKey();  
